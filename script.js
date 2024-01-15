0862aa0b396f4a320c13102d4efe885d0d0a6854
johncarlohexi <hexkundiman@gmail.com>
2024-01-15 22:32:41 +0900

    boxesのpをemploteeIDに繋げる

--- script.js
+++ script.js
@@ -6,13 +6,36 @@ let employeesId = [
 
 let emId;
 let emIdNum
+let res;
 function getId() {
     emId = document.getElementById("idBox").value
     emIdNum = Number(emId)
-    let res = employeesId.find(findId);
+    res = employeesId.find(findId);
     console.log(res)
 }
 
 function findId(item) {
     return item.id === emIdNum;
+}
+
+let getText = document.getElementsByClassName("boxes")
+
+for (let i = 0; i < getText.length; i++) {
+
+    let employeeName = getText[i].getElementsByClassName("employee-name")[0]
+    let employeeId = getText[i].getElementsByClassName("employee-id")[0]
+    let workingDays = getText[i].getElementsByClassName("working-days")[0]
+
+    if (employeeName) {
+        employeeName.innerHTML = "asd"
+    }
+
+    if (employeeId) {
+        employeeId.innerHTML = "sad"
+    }
+
+    if (workingDays) {
+        workingDays.innerHTML = "ss"
+    }
+    console.log(employeeName, employeeId, workingDays)
 }
