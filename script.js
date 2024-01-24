05ecd72028234d1414dd06f15a372e53591ea0ba
johncarlohexi <hexkundiman@gmail.com>
2024-01-24 16:31:21 +0900

    シンプルな作業員管理システム owari

--- script.js
+++ script.js
@@ -2,40 +2,71 @@ let employeesId = [
     {name:"Tanaka", id:101, workDays:20},
     {name:"Ella", id:102, workDays:18},
     {name:"Jason", id:103, workDays:17},
+    {name:"Pop", id:104, workDays:23},
+    {name:"Carlo", id:105, workDays:12},
+    {name:"Reigne", id:106, workDays:5},
 ]
 
 let emId;
 let emIdNum
 let res;
+
+/* employes details*/
+let employeeName = document.querySelector(".employee-name")
+let employeeId = document.querySelector(".employee-id")
+let employeeDays = document.querySelector(".working-days")
+
+/* Salary Calculator */
+
+let gakumen = document.querySelector(".gakumen")
+let baito = 10000;
+let seishain = 16250;
+
+/* Select Position */
+
+let getPosition;
+function selectP() {
+    getPosition = document.getElementById("position").value
+    console.log(getPosition)
+}
+
+
 function getId() {
     emId = document.getElementById("idBox").value
     emIdNum = Number(emId)
     res = employeesId.find(findId);
-    console.log(res)
+
+    if (emId == null || emId == "") {
+        alert("IDを入れてください。")
+    }
+
+    selectP()
+
+    if (getPosition === "baito") {
+        let baitoSalary = res.workDays * baito;
+        gakumen.innerHTML = baitoSalary
+        console.log(baitoSalary)
+    } else if (getPosition === "seishain") {
+        let seishainSalary = res.workDays * seishain;
+        gakumen.innerHTML = seishainSalary
+        console.log(seishainSalary)
+    } else {
+        gakumen.innerHTML = "役員を選択してください。"
+    }
+
+    if (res) {
+        employeeName.innerHTML = res.name
+        employeeId.innerHTML = res.id
+        employeeDays.innerHTML = res.workDays
+    } else {
+        employeeName.innerHTML = "IDを入れてください"
+        employeeId.innerHTML = "IDを入れてください"
+        employeeDays.innerHTML = "IDを入れてください"
+    }
+
+
 }
 
 function findId(item) {
     return item.id === emIdNum;
-}
-
-let getText = document.getElementsByClassName("boxes")
-
-for (let i = 0; i < getText.length; i++) {
-
-    let employeeName = getText[i].getElementsByClassName("employee-name")[0]
-    let employeeId = getText[i].getElementsByClassName("employee-id")[0]
-    let workingDays = getText[i].getElementsByClassName("working-days")[0]
-
-    if (employeeName) {
-        employeeName.innerHTML = "asd"
-    }
-
-    if (employeeId) {
-        employeeId.innerHTML = "sad"
-    }
-
-    if (workingDays) {
-        workingDays.innerHTML = "ss"
-    }
-    console.log(employeeName, employeeId, workingDays)
 }
